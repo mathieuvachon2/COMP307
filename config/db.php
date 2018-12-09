eef83b96884de3b9aa7d0ce3aee391e622e19910
SilanHe <silanhe@outlook.com>
2018-12-08 19:57:33 -0500

    Revert "started coding migrations"
    This reverts commit 1889cb55a9d1c3f9065821fa010ec0e1a3f3a70d.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=comp307',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'root1234',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
