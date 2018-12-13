800e0eee1c3eaf21ba6d4107e0bd2b1ebd57fa1e
mathieuvachon2 <vmat96@gmail.com>
2018-12-13 00:29:49 -0500

    descriptions

--- views/Games/table.php
+++ views/Games/table.php
@@ -2,7 +2,9 @@
     <?php foreach ($games as $game) { ?>
         <tr>
             <td><img class='game-image' src='<?php echo $game->image_url?>'/><td>
-            <td><?php echo $game->title." (".$game->year.")"?></td>
+            <td><?php echo $game->title." (".$game->year.")"?><br>
+            	<i class="italicDescription"><?php echo $game->description ?></i>
+            </td>
         </tr>
     <?php } ?>
 </table>
--- views/Movies/table.php
+++ views/Movies/table.php
@@ -2,7 +2,9 @@
     <?php foreach ($movies as $movie) { ?>
         <tr>
             <td><img class='game-image' src='<?php echo $movie->image_url?>'/><td>
-            <td><?php echo $movie->title.",".$movie->director->first_name." ".$movie->director->last_name."(".$movie->year.")"?></td>
+            <td><?php echo $movie->title.",".$movie->director->first_name." ".$movie->director->last_name."(".$movie->year.")"?><br>
+            	<i class="italicDescription"><?php echo $movie->description ?></i>
+            </td>
         </tr>
     <?php } ?>
 </table>
